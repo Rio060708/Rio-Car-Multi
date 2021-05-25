34247b248e9121e73beafcf82e05031363c5841a
Rio060708 <77329595+Rio060708@users.noreply.github.com>
2021-05-25 16:07:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,12 +13,12 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+  track = loadImage("https://rio060708.github.io/Rio-car-multi/images/track.jpg");
+  car1_img = loadImage("https://rio060708.github.io/Rio-car-multi/images/car1.png");
+  car2_img = loadImage("https://rio060708.github.io/Rio-car-multi/images/car2.png");
+  car3_img = loadImage("https://rio060708.github.io/Rio-car-multi/images/car3.png");
+  car4_img = loadImage("https://rio060708.github.io/Rio-car-multi/images/car4.png");
+  ground = loadImage("https://rio060708.github.io/Rio-car-multi/images/ground.png");
 }
 
 function setup(){
